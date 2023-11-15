1f715d2df4949bb951f161356f07aaf44ea60000
VioletaSW32 <erdelji.sw32.2019@uns.ac.rs>
2023-11-15 12:18:06 +0100

    Added psp front

---
+++ psp-front/src/modules/auth/auth.routes.ts
@@ -0,0 +1,16 @@
+import {Routes} from "@angular/router";
+
+export const AuthRoutes: Routes = [
+  // {
+  //   path: 'login',
+  //   pathMatch: 'full',
+  //   // component: LoginComponent,
+  //   // canActivate: [LoginGuard],
+  // },
+  // {
+  //   path: 'registration',
+  //   pathMatch: 'full',
+  //   // component: RegistrationComponent,
+  //   // canActivate: [LoginGuard],
+  // },
+]
